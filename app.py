bcf3a02acf4daf1c12c45dad9435bc5422329774
echixie <xiechenwei.ca@gmail.com>
2018-09-28 14:15:55 -0400

    echixie: update createProject

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 basedir = os.path.abspath(os.path.dirname(__file__))
 ALLOWED_EXTENSIONS = set(['xlsx', 'txt'])
 my_redis = CiqRedis()
+error = None
 
 
 @app.route('/', methods = ['GET', 'POST'])
@@ -22,24 +23,19 @@ def home():
 
 @app.route('/customer_list', methods = ['GET', 'POST'])
 def getCustomerList():
-    error = None
     m_customers = my_redis.get_set("customer")
     return render_template('customer_list.html', customers = m_customers, error = error)
 
 @app.route('/customer_project_list')
 def getCustomerProject():
-    error = None
-    var_str = request.url.__str__().rsplit('?')
-    m_customer = var_str[1].rsplit('=')[1]
+    param = request.url.__str__().rsplit('?')[1]
+    m_customer = param.rsplit('=')[1]
     m_projects = my_redis.get_set(m_customer)
-    #m_projects = ["vIMS1.6", "ECC"]
     return render_template('customer_project_list.html', customer=m_customer, projects=m_projects, error = error)
 
 @app.route('/customer_node_list')
 def getCustomerNode():
-    error = None
-    var_str = request.url.__str__().rsplit('?') 
-    params = var_str[1].rsplit('&')
+    params = request.url.__str__().rsplit('?')[1].rsplit('&')
     m_customer = params[0].rsplit('=')[1]
     m_project = params[1].rsplit('=')[1]
     m_nodes = my_redis.get_set(m_customer + m_project)
@@ -52,34 +48,31 @@ def getCustomerNode():
 
 @app.route('/api/dump', methods=['POST'], strict_slashes=False)
 def dumpExcel():
-    error = None
     print("api_dump")
-    var_str = request.url.__str__().rsplit('?')
-    m_customer = var_str[1].rsplit('&')[0].rsplit('=')[1]
-    m_project = var_str[1].rsplit('&')[1].rsplit('=')[1]
+    params = request.url.__str__().rsplit('?')[1].rsplit('&')
+    m_customer = params[0].rsplit('=')[1]
+    m_project = params[1].rsplit('=')[1]
     m_ciqData = CiqData(m_customer, m_project)
     m_ciq = m_ciqData.fetch_ciq_from_redis()
     m_ciqData.dump_to_excel(m_ciq)
     return render_template('alert.html', message="Dump data to excel file successfully",error=error)
 
-@app.route('/customer_node_detail')
-def getNodeDetail():
-    error = None
-    var_str = request.url.__str__().rsplit('?')
-    m_customer = var_str[1].rsplit('&')[0].rsplit('=')[1]
-    m_project = var_str[1].rsplit('&')[1].rsplit('=')[1]
-    m_node = var_str[1].rsplit('&')[2].rsplit('=')[1]
-    m_attrs = my_redis.get_hash(m_customer + m_project + m_node)
-    return render_template('customer_node_detail.html', customer=m_customer, project=m_project, node=m_node, attrs=m_attrs,error =error)
+# @app.route('/customer_node_detail')
+# def getNodeDetail():
+#     params = request.url.__str__().rsplit('?')[1].rsplit('&')
+#     m_customer = params[0].rsplit('=')[1]
+#     m_project = params[1].rsplit('=')[1]
+#     m_node = params[2].rsplit('=')[1]
+#     m_attrs = my_redis.get_hash(m_customer + m_project + m_node)
+#     return render_template('customer_node_detail.html', customer=m_customer, project=m_project, node=m_node, attrs=m_attrs,error =error)
 
 @app.route('/customer_node_modify',methods=['POST'])
 def modifyNodeDetail():
-    error = None
-    var_str = request.url.__str__().rsplit('?')
+    params = request.url.__str__().rsplit('?')[1].rsplit('&')
    
-    m_customer = var_str[1].rsplit('&')[0].rsplit('=')[1]
-    m_project = var_str[1].rsplit('&')[1].rsplit('=')[1]
-    m_node = var_str[1].rsplit('&')[2].rsplit('=')[1]
+    m_customer = params[0].rsplit('=')[1]
+    m_project = params[1].rsplit('=')[1]
+    m_node = params[2].rsplit('=')[1]
     m_attrs = my_redis.get_hash(m_customer + m_project + m_node)
     new_attrs ={}
 
@@ -87,7 +80,7 @@ def modifyNodeDetail():
         new_attrs[key] = request.form[m_node+key]
     my_redis.set_m_hash(new_attrs, m_customer + m_project + m_node)
 
-    m_nodes = (my_redis.get_set(m_customer+m_project))
+    m_nodes = my_redis.get_set(m_customer+m_project)
     m_nodes.remove(m_node)
     m_dict = {}
     for node in m_nodes:
@@ -96,21 +89,20 @@ def modifyNodeDetail():
 
 
 
-@app.route('/customer_project_detail')
-def getProjectInfo():
-    error = None
-    var_str = request.url.__str__().rsplit('?')
-    m_customer = var_str[1].rsplit('&')[0].rsplit('=')[1]
-    m_project = var_str[1].rsplit('&')[1].rsplit('=')[1]
-    #mtas = {"oam_vip":"10.0.0.10", "traffic_vip":"10.0.0.11"}
-    #sbg = {"mip": "10.0.0.12", "access_ip":"10.0.0.13"}
-    #m_ciq = {"mtas": mtas, "sbg":sbg}
-    m_ciq = my_redis.get_data(m_customer)[m_project]
-    return render_template('customer_project_detail.html', customer=m_customer, project=m_project, ciq=m_ciq, error = error)
+# @app.route('/customer_project_detail')
+# def getProjectInfo():
+#     error = None
+#     var_str = request.url.__str__().rsplit('?')
+#     m_customer = var_str[1].rsplit('&')[0].rsplit('=')[1]
+#     m_project = var_str[1].rsplit('&')[1].rsplit('=')[1]
+#     #mtas = {"oam_vip":"10.0.0.10", "traffic_vip":"10.0.0.11"}
+#     #sbg = {"mip": "10.0.0.12", "access_ip":"10.0.0.13"}
+#     #m_ciq = {"mtas": mtas, "sbg":sbg}
+#     m_ciq = my_redis.get_data(m_customer)[m_project]
+#     return render_template('customer_project_detail.html', customer=m_customer, project=m_project, ciq=m_ciq, error = error)
 
 @app.route('/alert')
 def showAlert(message):
-    error = None
     m_message = message
     return render_template('alert.html', message=m_message, error = error)
 
@@ -146,9 +138,9 @@ def upload(customer, project):
 def api_upload():
     #file_dir = os.path.join(basedir, m_customer)
     print("api_upload")
-    var_str = request.url.__str__().rsplit('?')
-    m_customer = var_str[1].rsplit('&')[0].rsplit('=')[1]
-    m_project = var_str[1].rsplit('&')[1].rsplit('=')[1]
+    params = request.url.__str__().rsplit('?')[1].rsplit('&')
+    m_customer = params[0].rsplit('=')[1]
+    m_project = params[1].rsplit('=')[1]
     print(m_customer)
     print(m_project)
 
@@ -173,7 +165,10 @@ def api_upload():
                 my_redis.set_set(key, m_customer+m_project)
                 my_redis.set_m_hash(value, m_customer+m_project+key)
 
-        return  render_template("customer_node_list.html", customer=m_customer, project=m_project, nodes=my_ciq.nodesConf.keys())
+        m_nodes = my_ciq.nodesConf.items()
+        m_active_node, m_active_attrs = m_nodes.pop()
+        return render_template('customer_node_list2.html', customer=m_customer, project=m_project, nodes=m_nodes, active_node=m_active_node, active_attrs=m_active_attrs, error =error)
+
     else:
         return jsonify({"errno": 1001, "errmsg": "CIQ parse failed"})
 #        return jsonify({"filename": f.filename})
