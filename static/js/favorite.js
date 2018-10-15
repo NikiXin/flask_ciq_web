107103f9493a2e2038cd66eb04050d40f37eae16
NikiXin <301956646@qq.com>
2018-10-15 16:08:18 +0000

    set navbar

--- static/js/favorite.js
+++ static/js/favorite.js
@@ -1,19 +1,27 @@
+var data = [];
+var num = 0;
+
 document.getElementById("tools").addEventListener("click",function(e) {
-
-  var data = [];
-  var num = 0;
-
-  // for (var num =0; num <3; num++){
-  //   if(e.target && e.target.nodeName){
-  //     data.push(e.target.id)
-  //   }
-  // }
 
   if(e.target && e.target.nodeName == "LI" && num <3) {
       console.log(e.target.id + " was clicked");
       data.push(e.target.id);
       num = num+1;
   }
+  console.log(num);
   console.log(data);
  
+  if (num ==3){
+    $.ajax({
+      type : "POST",
+      url : "/engine/<username>",
+      contentType : "application/json",
+      data : JSON.stringify(data),
+      dataType: "json",
+      async: false,
+      success: function(data) {
+          alert("success: " + data);
+      }
+  });
+  }
 });
